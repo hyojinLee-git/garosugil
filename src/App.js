2cc051f0ee7ccf71d075777c4dce9191868ac3e6
hyojinLee-git <lily960310@naver.com>
2021-11-11 12:27:11 +0900

    modify:build file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import CaretreeIntroduce from './pages/CaretreeIntroduce';
-import DataVisualization from './pages/DataVisualization';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Link to="/caretreeintroduce">introduce</Link>
-        <Routes>
-          <Route path="/caretreeintroduce" element={<CaretreeIntroduce />} />
-          <Route path="/datavisualization" element={<DataVisualization />} />
-        </Routes>
-      </Router>
+      <CaretreeIntroduce />
     </div>
   );
 }
