bf9ec52bef923db3202a50c9c3c0fd1750b6b8ef
hyojinLee-git <lily960310@naver.com>
2021-11-11 11:46:34 +0900

    chore:build file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CaretreeIntroduce from './pages/CaretreeIntroduce';
 import DataVisualization from './pages/DataVisualization';
 
