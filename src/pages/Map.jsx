b2006e6ec51e0c84952377663bbfcd12c3f3c10b
hyojinLee-git <lily960310@naver.com>
2021-11-26 00:56:38 +0900

    feat: map page

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect} from 'react';
 import {RenderAfterNavermapsLoaded, NaverMap} from 'react-naver-maps'
 import axios from 'axios'
 
